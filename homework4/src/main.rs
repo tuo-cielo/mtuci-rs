a5f223cd5eddd9225b9e1db64e5e65b84f9ab950
SUS <olgagorhkovs@gmail.com>
2023-07-10 19:30:18 +0300

    --

--- homework4/src/main.rs
+++ homework4/src/main.rs
@@ -1,3 +1,25 @@
 
+fn main() {
+    //Метод new и with capacity
+    let mut xs: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
+
+    //push
+
+
+
+    //pop
+
+    //remove
+
+
+    // get
+    let first = xs.get(0).unwrap();
+    let first_two = xs.get(0..2).unwrap();
+
+    println!("The first element is: {}", first);
+    println!("The first two elemetns: {:?}", first_two);
+
+    //resize
+}
 
 //  ๐·°(৹˃̵﹏˂̵৹)°·๐
