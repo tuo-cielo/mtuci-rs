fdef6bc22fd247c7b7e2bbabdbd616a93656ff06
SUS <olgagorhkovs@gmail.com>
2023-07-21 12:42:05 +0300

    --

--- telebot/src/main.rs
+++ telebot/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     // Создаём бота
     let mut bot = Bot::new(&env::var("6398684315:AAHUZolIBxTKM5i0IU3QzuFVocn1-Vn2PVo").unwrap()).update_interval(200);
  
-    // Register a reply command which answers a message
+    // Комманда с ответным сообщением
     let handle = bot.new_cmd("/reply")
         .and_then(|(bot, msg)| {
             let mut text = msg.text.unwrap().clone();
